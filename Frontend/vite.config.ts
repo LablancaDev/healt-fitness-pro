17b5e4d7ceb2f9417e03bbe0ce680fd7ab5dbaf7
LablancaDev <valenciano_david@hotmail.com>
2024-11-05 16:40:54 +0100

    update endpoints

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,10 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  }
+  },
+  server: {
+    host:true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
 
