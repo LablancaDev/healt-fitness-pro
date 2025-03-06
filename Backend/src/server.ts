85e7e32f396822143d2f945a67b99e5ed170d8b2
LablancaDev <valenciano_david@hotmail.com>
2025-03-06 11:44:35 +0100

    change deployment vercel to render

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -26,26 +26,13 @@ app.use((req, res, next) => {
 });
 
 // Configurar CORS para Render y entorno local
-const allowedOrigins = [
-    'https://frontend-healt-fitness-pro.onrender.com',  // Frontend en producción
-    'https://healt-fitness-pro.onrender.com',
-    'http://localhost:5173',                            // Frontend local para desarrollo (si usas Vite)
-    process.env.FRONTEND_URL                            // URL del frontend configurado en las variables de entorno
-].filter(Boolean); // 🔹 Filtra valores undefined
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            console.error(`CORS error: Origin not allowed: ${origin}`);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+
 
 // Middleware para interpretar JSON
 app.use(express.json());
