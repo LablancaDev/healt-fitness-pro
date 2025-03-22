e0e5603929c5448c8ed3417ef8256b2c7a30f9e2
LablancaDev <valenciano_david@hotmail.com>
2025-03-22 18:41:12 +0100

    update component footer

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -3,7 +3,20 @@ import logo from '../assets/imgs/interface2.png'
 const Footer = () => {
   return (
     <div className="p-4 text-center footer">
-        <a className="navbar-brand title " href="#">Healt<img className='logo' src={logo} alt="logo" />FitnessPro</a>
+      <a className="navbar-brand title " href="#">Healt<img className='logo' src={logo} alt="logo" />FitnessPro</a>
+
+      <p className="mb-0 text-light">
+        &copy; {new Date().getFullYear()} | Developed by{" "}
+        <a
+          href="https://lablancadev.github.io/My-portfolio/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-info"
+        >
+          David Risueño Lablanca
+        </a>
+      </p>
+
     </div>
   )
 }
